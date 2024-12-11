77292805418a05b5b0a9ae583230b61663dfc8e3
nourhane-dokma <nournourahmed1980@gmail.com>
2024-12-11 12:03:43 +0100

    Update

---
+++ tp2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+    char M[5][5]= {{'1','2','3','4','5'},{'7','a','c','8','d'},{'c','9','4','z','8'},
+        {'5','6','p','n','3'},{'2','9','t','m','k'}
+    };
+    int i,j;
+    for (i=0; i<5; i++) {
+        for (j=0; j<5; j++) {
+            printf("%c ",M[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i=0; i<5; i++) {
+        for (j=0; j<5; j++) {
+            if (i%2==0)
+                printf(" %c",M[i][j]);
+        }
+        printf(" \n ");
+        
+        for(j=0;j<5;j++){
+        if(j%2 != 0)
+        printf("%c",M[i][j]);}
+    }
+    return 0;
+}
